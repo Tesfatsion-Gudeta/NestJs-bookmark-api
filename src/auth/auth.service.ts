45c71a1810b51bd78405e2a12cc8ee0abc7e1aba
tesfatsion <tesfatsiongudeta7@gmail.com>
2025-04-01 23:47:21 -0700

    refactor: update database schema and implement user authentication with argon2

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { AuthDto } from './dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import * as argon from 'argon2';
 
 @Injectable()
 export class AuthService {
-  signup(dto: AuthDto) {
-    console.log({ dto });
-    return { msg: 'signed up' };
+  constructor(private prisma: PrismaService) {}
+  async signup(dto: AuthDto) {
+    const hash = await argon.hash(dto.password);
+
+    const user = await this.prisma.user.create({
+      data: {
+        email: dto.email,
+        hash,
+      },
+    });
+    // delete user.hash;
+    return user;
   }
   signin() {
     return { msg: 'signed in' };
