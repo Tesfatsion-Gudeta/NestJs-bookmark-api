7ee53b004e9c5711923068a8cc1db0b01484f59e
Tesfatsion Gudeta <tesfatsiongudeta7@gmail.com>
2025-07-24 02:46:47 +0300

    feat: update CORS configuration to allow multiple frontend origins

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173', // Allow your frontend origin
+    origin: [
+      'http://localhost:5173',
+      'https://nestjs-bookmark-api-wloh.onrender.com',
+    ], // Allow your frontend origins
     credentials: true, // If you are sending cookies or auth headers
   });
 
